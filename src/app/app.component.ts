a4f2d2dd642f13b7b1517a2cddb3af90dc562a67
ani devdariani <101930091+devdariannianni@users.noreply.github.com>
2024-01-06 23:19:32 +0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,27 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-app';
+  
+  showSmiley: boolean = false;
+  showThanksMessage: boolean = false; // Add this variable
+  noButtonPosition: any = { top: '0', left: '0' };
+  noButtonInitialPosition: any = {}; 
+  
+  onYesClick() {
+    this.showSmiley = true;
+    this.showThanksMessage = true; // Display the thanks message
+  }
+  
+  onNoHover() {
+    if (!this.noButtonInitialPosition.top) {
+      this.noButtonInitialPosition = { ...this.noButtonPosition };
+    }
+    this.generateRandomPosition();
+  }
+  
+  generateRandomPosition() {
+    const randomTop = Math.floor(Math.random() * window.innerHeight);
+    const randomLeft = Math.floor(Math.random() * window.innerWidth);
+    this.noButtonPosition = { top: `${randomTop}px`, left: `${randomLeft}px` };
+  }
 }
